6afce7394cc55fc36f0ce63319c0e86c19dda0ea
Lucas Speers <lucas_speers@outlook.com>
2025-02-01 14:53:03 -0700

    correct pin

--- src/gpio.rs
+++ src/gpio.rs
@@ -2,7 +2,7 @@ use std::{sync::mpsc::Sender, thread, time::{Duration, Instant}};
 
 use rppal::gpio::Gpio;
 
-const GPIO_PIN: u8 = 24;
+const GPIO_PIN: u8 = 15;
 
 pub fn run(tx: Sender<()>) {
   let mut pin = Gpio::new().unwrap().get(GPIO_PIN).unwrap().into_input();
