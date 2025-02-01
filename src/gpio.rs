7e6b528c99b1fd054c58f19664c6d4253f0fbc7d
Lucas Speers <lucas_speers@outlook.com>
2025-02-01 14:45:23 -0700

    test

--- src/gpio.rs
+++ src/gpio.rs
@@ -2,26 +2,13 @@ use std::{sync::mpsc::Sender, thread, time::{Duration, Instant}};
 
 use rppal::gpio::Gpio;
 
-const GPIO_PIN_TRIGGER: u8 = 23;
-const GPIO_PIN_ECHO: u8 = 24;
+const GPIO_PIN: u8 = 24;
 
 pub fn run(tx: Sender<()>) {
-  let mut trigger = Gpio::new().unwrap().get(GPIO_PIN_TRIGGER).unwrap().into_output();
-  let mut echo = Gpio::new().unwrap().get(GPIO_PIN_ECHO).unwrap().into_input();
+  let mut pin = Gpio::new().unwrap().get(GPIO_PIN).unwrap().into_input();
 
   loop {
-    trigger.set_low();
-    thread::sleep(Duration::from_micros(5));
-    trigger.set_high();
-    thread::sleep(Duration::from_micros(10));
-    trigger.set_low();
-    println!("triggered");
-    let now = Instant::now();
-    while echo.is_high() {}
-    println!("went low");
-    let time_elapsed = now.elapsed();
-
-    println!("{:?}", time_elapsed);
+    println!("{:?}", pin.is_high());
 
     thread::sleep(Duration::from_secs(1));
     tx.send(()).unwrap();
