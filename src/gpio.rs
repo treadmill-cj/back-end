077f1dd0bbba1a7f1d9467aca46ae4926b3bdb2f
Lucas Speers <lucas_speers@outlook.com>
2025-02-01 12:45:09 -0700

    testing

--- src/gpio.rs
+++ src/gpio.rs
@@ -1,11 +1,14 @@
 use std::{sync::mpsc::Sender, thread, time::Duration};
 
+use rppal::gpio::Gpio;
+
 // use gpio::GpioIn;
 
 // const GPIO_PIN_TRIGGER: u16 = 23;
 // const GPIO_PIN_ECHO: u16 = 24;
 
 pub fn run(tx: Sender<()>) {
+  let mut pin = Gpio::new().unwrap().get(23).unwrap().into_input();
   // let mut gpio_trigger =
   //   gpio::sysfs::SysFsGpioInput::open(GPIO_PIN_TRIGGER)
   //   .expect("Could not open trigger gpio pin");
@@ -15,6 +18,7 @@ pub fn run(tx: Sender<()>) {
   //   .expect("Could not open echo gpio pin");
 
   loop {
+    println!("{:?}", pin.is_high());
     // let value_trigger = gpio_trigger.read_value().expect("Could not read gpio pin");
     // let value_echo = gpio_echo.read_value().expect("Could not read gpio pin");
     // println!("{value_trigger:?}, {value_echo:?}");
