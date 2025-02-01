25fef31c00c8996426dbb7442e38cc256fd0e4d6
Lucas Speers <lucas_speers@outlook.com>
2025-02-01 12:38:21 -0700

    better cors headers

--- src/api.rs
+++ src/api.rs
@@ -23,7 +23,9 @@ pub fn run(calculated_data: Arc<Mutex<Vec<CalcData>>>) {
           total_str.pop(); // remove last comma
           total_str += "]";
           let h = Header::from_str("Access-Control-Allow-Origin: *").unwrap();
-          request.respond(Response::from_string(total_str).with_header(h)).unwrap()
+          let h2 = Header::from_str("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS").unwrap();
+          let h3 = Header::from_str("Access-Control-Allow-Headers: Content-Type, Authorization").unwrap();
+          request.respond(Response::from_string(total_str).with_header(h).with_header(h2).with_header(h3)).unwrap()
         },
         _ => request.respond(Response::from_string("404")).unwrap()
       }
