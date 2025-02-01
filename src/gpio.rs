d4319836372a12a047bb76b3c230948f5b9de53b
Lucas Speers <lucas_speers@outlook.com>
2025-02-01 15:01:12 -0700

    refined time

--- src/gpio.rs
+++ src/gpio.rs
@@ -11,8 +11,8 @@ pub fn run(tx: Sender<()>) {
     while pin.is_high() {} // wait
     tx.send(()).unwrap();
     println!("BAM!!!");
-    thread::sleep(Duration::from_millis(500));
+    thread::sleep(Duration::from_millis(200));
     while pin.is_low() {} // wait to reset
-    thread::sleep(Duration::from_millis(500));
+    thread::sleep(Duration::from_millis(200));
   }
 }
